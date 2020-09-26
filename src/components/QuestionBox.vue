a72489db88f1048cc07915cb3af868d35ecf2d9a
cyrdodi <cyrillusdodi@gmail.com>
2020-09-26 20:42:49 +0700

    some of this some of that

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <b-jumbotron>
+      <template slot:lead> </template>
+      <p v-html="currentQuestion.question"></p>
+      <hr class="my-4" />
+
+      <p>{{ currentQuestion.correct_answer }}</p>
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button @click="next" variant="success" href="#"> Next </b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    currentQuestion: Object,
+    next: Function,
+  },
+};
+</script>
